bfad7c58e06860574ebfc316075252e7a053869a
Michelle A. Zee <maz2136@columbia.edu>
2021-04-11 13:58:24 -0400

    add main, construction, text tabs

--- dashboard/dashboard_code.R
+++ dashboard/dashboard_code.R
@@ -49,8 +49,21 @@ age_lab <- "Median Age"
 # --------------------- dashboard w/ sidebar panel ----------------------
 
 ## -------------------- UI
-ui <- navbarPage("NYC Construction",
+ui <- navbarPage("Manhattan Construction",
+                 
                  tabPanel("Main",
+                   mainPanel(
+                     h2("Manhattan Construction and Neighborhood Changes Over Time"),
+                     h5("By Melissa S Feeney, Catherine Chen, Natalie Weng, Michelle A. Zee"),
+                     p("This project examines the Manhattan residential and commercial building construction permits 
+                     and associated changes in neighborhoods from 2009 to 2019. Data used include construction permit 
+                     data from the NYC Department of Buildings, demographic and home price data from the American 
+                     Community Survey, and neighborhood descriptions from Wikipedia.
+                       ")
+                   )
+                 ),
+                 
+                 tabPanel("Construction",
                           mainPanel(
                             fluidRow(
                               box(width = 12, 
@@ -64,18 +77,11 @@ ui <- navbarPage("NYC Construction",
                               box(width = 12, 
                                   
                                   leafletOutput("p_const_alt_map")
-                              ),
-                              
-                              br(),
-                              br(),
-                              
-                              box(width = 12, 
-                                  
-                                  leafletOutput("p_const_demo_map")
+                              )
                               )
                             )
-                          )
-                 ),
+                          ),
+
                  tabPanel("Neighborhood Demographics",
                           sidebarLayout(
                             sidebarPanel(
@@ -95,10 +101,12 @@ ui <- navbarPage("NYC Construction",
                             mainPanel(
                               fluidRow(
                                 plotOutput("p_med_age")
-                              )
-                            )
-                          )
-                 )
+                                        )
+                                      )
+                                    )
+                          ),
+                 
+                 tabPanel("Neighborhoods in Words")
 )
 
 ## -------------------- server
@@ -179,159 +187,8 @@ server <- function(input, output) {
     post2010_map
     
   })
-  
-  output$p_const_demo_map <- renderLeaflet({
-    # Total Map
-    # Prep Pop-up details
-    popup_content <- paste('Job Number:',post2010$Job_Number,'<br/>',
-                           'Job Status:',post2010$Job_Status,'<br/>',
-                           'Initial Occupier:',post2010$Occ_Init,'<br/>',
-                           'Proposed Occupier:',post2010$Occ_Prop,'<br/>',
-                           'Landmark Status:',post2010$Landmark,'<br/>',
-                           'Building Ownership:',post2010$Ownership,'<br/>',
-                           'PUMA:',post2010$PUMA2010,'<br/>')
-    
-    # Map Title
-    map_title <- tags$p(tags$style('p {color: black; font-size: 20px}'),
-                        tags$b('Demolition'))
-    
-    post2010 <- post2010 %>% filter(CompltYear == 2020,
-                                    Job_Type == "Demolition")
-    
-    post2010_map <- leaflet(post2010) %>%
-      addTiles() %>%
-      addProviderTiles(providers$Wikimedia) %>% 
-      
-      addCircleMarkers(popup = popup_content,
-                       clusterOptions = markerClusterOptions()) %>%
-      
-      # Add map title
-      addControl(map_title, position = 'topright')
-    
-    post2010_map
-    
-  })
 }
 
 shinyApp(ui, server)
-
-
-
-
-
-
-
-
-# --------------------- dashboard with melissa's original map ----------------------
-## -------------------- UI
-ui <- navbarPage("NYC Construction",
-             tabPanel("Main",
-                      mainPanel(
-                        fluidRow(
-                          leafletOutput(("p_const_map"))
-                        )
-                      )
-                      ),
-             tabPanel("Neighborhood Demographics",
-                      sidebarLayout(
-                        selectInput("puma",
-                                    label = "Choose NYC Neighborhood:",
-                                    choices = input_puma,
-                                    selected = "Manhattan"
-                                    ),
-                        sliderInput("year",
-                                    label = "Select Year:",
-                                    value = 2012, min = 2009, max = 2019,
-                                    step = 1,
-                                    ticks = FALSE,
-                                    sep = "")),
-                        mainPanel(
-                          fluidRow(
-                            plotOutput("p_med_age")
-                                    )
-                                  )
-                      )
-             )
-  
-## -------------------- server
-server <- function(input, output) {
-  output$p_med_age <- renderPlot({
-    age <- subset(med_age,
-                  year <= input$year &
-                #  borough == input$borough
-                  pumaname10 == input$puma)
-    ggplot(age, aes(year, estimate, color = as.factor(pumaname10))) +
-      geom_line() +
-      scale_x_continuous(breaks= c(2009:2019)) +
-      labs(x = year_lab, y = age_lab,
-           title = "Median Age by PUMA") +
-      theme_minimal()
-  })
-  
-  output$p_const_map <- renderLeaflet({
-    # Total Map
-    # Prep Pop-up details
-    popup_content <- paste('Job Number:',post2010$Job_Number,'<br/>',
-                           'Job Status:',post2010$Job_Status,'<br/>',
-                           'Initial Occupier:',post2010$Occ_Init,'<br/>',
-                           'Proposed Occupier:',post2010$Occ_Prop,'<br/>',
-                           'Landmark Status:',post2010$Landmark,'<br/>',
-                           'Building Ownership:',post2010$Ownership,'<br/>',
-                           'PUMA:',post2010$PUMA2010,'<br/>')
-    
-    # Map Title
-    map_title <- tags$p(tags$style('p {color: black; font-size: 20px}'),
-                        tags$b('Construction Across NYC'))
-    
-    # Color Palette 1 for the Map: Job Type
-    pal1 = colorFactor('Set1', domain = post2010$Job_Type) 
-    color_Job_Type = pal1(post2010$Job_Type)
-    
-    # Color Palette 2 for the Map: Job Completion Year
-    pal2 = colorFactor('Paired', domain = post2010$CompltYear) 
-    color_CompltYear = pal2(post2010$CompltYear)
-    
-    # Color Palette 3 for the Map: Boro
-    pal3 = colorFactor('Dark2', domain = post2010$boro) 
-    color_boro = pal3(post2010$boro)
-    
-    post2010 <- post2010 %>% filter(CompltYear == 2020) ######### do we just want to use 1 year of data?
-    
-    post2010_map <- leaflet(post2010) %>%
-      addTiles() %>%
-      addProviderTiles(providers$Wikimedia) %>% 
-      
-      # Add Borough Name Data
-      addCircles(color = color_boro,
-                 popup = popup_content,
-                 group = 'Toggle: Borough') %>%
-      addLegend(pal = pal3, values = ~post2010$boro, title = 'Project Borough', position = 'bottomright') %>%
-
-      # Add Job Type Data
-      addCircles(color = color_Job_Type, 
-                 popup = popup_content,
-                 group = 'Toggle: Job Type') %>%
-      addLegend(pal = pal1, values = ~post2010$Job_Type, title = 'Job Type', position = 'bottomright') %>%
-      
-      # # Add Completion Year Data
-      addCircles(color = color_CompltYear,
-                 popup = popup_content,
-                 group = 'Toggle: Year of Project Completion') %>%
-      addLegend(pal = pal2, values = ~post2010$CompltYear, title = 'Year of Project Completion', position = 'bottomright') %>%
-
-      # Layers to add toggle ability
-      addLayersControl(baseGroups = c('Toggle: Job Type', 'Toggle: Year of Project Completion', 'Toggle: Borough'),
-                       options = layersControlOptions(collapsed = FALSE), position = 'bottomright') %>%
-      
-      # Add map title
-      addControl(map_title, position = 'topright')
-    
-    post2010_map
-    
-  })
-}
-
-shinyApp(ui, server)
-
 
 
